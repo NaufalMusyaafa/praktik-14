1216c6b712e6bff7820632bcee1784b016c6dfa4
Naufal Musyaafa <naufal@musyaafa.com>
2025-12-10 23:41:59 +0700

    update config for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/praktik-14/",
+  // base: "/praktik-14/",
   plugins: [react()],
 });
